9d5933d246cfc3125b73bf678aab382cebe7ed19
eGalv3zDev <acamposeco-2024018@kinal.edu.gt>
2025-10-06 19:11:07 -0600

    correciones para funcionamiento de proyecto

--- WorkAS/src/org/workas/system/Main.java
+++ WorkAS/src/org/workas/system/Main.java
@@ -18,11 +18,12 @@ import org.workas.controller.InicioSesionController;
  * @author enriq
  */
 public class Main extends Application {
+
     private static String URL_VIEW = "/org/workas/view/";
     private static String URL = "/org/workas/";
     private Stage escenarioPrincipal;
     private Scene escena;
-    
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -30,40 +31,40 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         this.escenarioPrincipal = stage;
-        
+
         inicio();
         escenarioPrincipal.setScene(escena);
         escenarioPrincipal.getIcons().add(new Image(URL + "image/Logo.png"));
         escenarioPrincipal.setResizable(false);
         escenarioPrincipal.setTitle("WorkAS");
-        escenarioPrincipal.show();       
-    }  
-    
-    public Initializable cambiarEscena(String fxml, double ancho, double alto) throws Exception{
+        escenarioPrincipal.show();
+    }
+
+    public Initializable cambiarEscena(String fxml, double ancho, double alto) throws Exception {
         Initializable interfazCargada = null;
-        
+
         FXMLLoader cargadorFXML = new FXMLLoader();
-        
-        InputStream archivoFXML = Main.class.getResourceAsStream(URL_VIEW+fxml);
+
+        InputStream archivoFXML = Main.class.getResourceAsStream(URL_VIEW + fxml);
         cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
-        cargadorFXML.setLocation(Main.class.getResource(URL_VIEW+fxml));                                
-        
-        escena = new Scene(cargadorFXML.load(archivoFXML), ancho, alto);        
-        escenarioPrincipal.setScene(escena);        
+        cargadorFXML.setLocation(Main.class.getResource(URL_VIEW + fxml));
+
+        escena = new Scene(cargadorFXML.load(archivoFXML), ancho, alto);
+        escenarioPrincipal.setScene(escena);
         escenarioPrincipal.setResizable(true);
         escenarioPrincipal.sizeToScene();
         escenarioPrincipal.sizeToScene();
-        
+
         interfazCargada = cargadorFXML.getController();
         return interfazCargada;
     }
-    
-    public void inicio(){
-        try{
-            InicioSesionController ic = (InicioSesionController)cambiarEscena("InicioSesion.fxml", 722, 400);        
+
+    public void inicio() {
+        try {
+            InicioSesionController ic = (InicioSesionController) cambiarEscena("InicioSesion.fxml", 722, 400);
             ic.setPrincipal(this);
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }        
+        }
     }
 }
